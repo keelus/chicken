37df4ae08c5034839e272abcd2c35fee539b1244
keelus <hugomoreda@hotmail.com>
2025-08-18 20:38:07 +0200

    Fix server bound string parsing.

--- src/net/server_bound/fields/string.h
+++ src/net/server_bound/fields/string.h
@@ -3,16 +3,17 @@
 
 #include "../../common/aliases.h"
 #include "varint.h"
+#include <stdio.h>
 
 static inline char *net_sb_string_parse(const char *buffer, const usize len,
 										usize *index) {
 	usize string_len = net_sb_varint_parse(buffer, len, index);
 	char *s = (char *)malloc(sizeof(char) * (string_len + 1));
-	for(usize i = 0; i < len; i++) {
+	for(usize i = 0; i < string_len; i++) {
 		s[i] = buffer[*index + i];
 	}
-	s[len] = '\0';
-	*index += len;
+	s[string_len] = '\0';
+	*index += string_len;
 	return s;
 }
 
